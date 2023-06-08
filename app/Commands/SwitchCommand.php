03acbc695f4201fa512e54153514f17ba4d89b91
adam <brown-a2@users.noreply.github.com>
2023-06-08 16:11:11 +0100

    Add switch command to change between namespaces

---
+++ app/Commands/SwitchCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class SwitchCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'switch { target : Environment you are switching to. Options are, prod, staging, dev, demo} ';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Switch between different environments.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $target = $this->argument('target');
+        $namespace = "hale-platform-$target";
+        passthru("kubectl config set-context --current --namespace=$namespace");
+        $this->info('Switched to: ' . $namespace);
+        passthru("kubectl get all");
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
