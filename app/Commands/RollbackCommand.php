a4100fa4823860b8dcdad40bb2d99aed3c368c76
adam <brown-a2@users.noreply.github.com>
2023-06-08 21:58:51 +0100

    Standardize file naming

--- app/Commands/RollbackCommand.php
+++ app/Commands/RollbackCommand.php
@@ -5,7 +5,7 @@ namespace App\Commands;
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
 
-class Rollback extends Command
+class RollbackCommand extends Command
 {
     /**
      * The signature of the command.
