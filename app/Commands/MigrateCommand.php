82025d0548c1ee3adec6fad9563e1c4e805a2a32
adam <brown-a2@users.noreply.github.com>
2023-06-09 16:41:45 +0100

    Divide migrate class into separate functions for local and CP

--- app/Commands/MigrateCommand.php
+++ app/Commands/MigrateCommand.php
@@ -12,14 +12,119 @@ class MigrateCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'migrate { source : Environment you are migrating from. } { target : Environment you are copying to. }';
+    protected $signature = 'migrate { source : Environment you are migrating from. } { target : Environment you are migrating to. Options are prod, staging, dev, demo. }';
 
     /**
      * The description of the command.
      *
      * @var string
      */
-    protected $description = 'Migrate to the local environment';
+    protected $description = 'Migrate site(s) from one environment to another, prod, staging, dev, demo';
+
+    /**
+     * The source environment.
+     *
+     * @var string|null
+     */
+    protected $source;
+
+    /**
+     * The target environment.
+     *
+     * @var string|null
+     */
+    protected $target;
+
+    /**
+     * The namespace.
+     *
+     * @var string|null
+     */
+    protected $namespace;
+
+    /**
+     * The pod name.
+     *
+     * @var string|null
+     */
+    protected $podName;
+
+    /**
+     * The pod execution command.
+     *
+     * @var string|null
+     */
+    protected $podExec;
+
+    /**
+     * The SQL file name.
+     *
+     * @var string|null
+     */
+    protected $sqlFile;
+
+    /**
+     * The path.
+     *
+     * @var string|null
+     */
+    protected $path;
+
+    /**
+     * The secret name for S3 bucket.
+     *
+     * @var string|null
+     */
+    protected $secretName;
+
+    /**
+     * The secrets from decode-secret command.
+     *
+     * @var string|null
+     */
+    protected $secrets;
+
+    /**
+     * The decoded JSON secrets.
+     *
+     * @var object|null
+     */
+    protected $json_secrets;
+
+    /**
+     * The S3 bucket name.
+     *
+     * @var string|null
+     */
+    protected $bucket;
+
+    /**
+     * The uploads path.
+     *
+     * @var string|null
+     */
+    protected $uploadsPath;
+
+    /**
+     * The profile name.
+     *
+     * @var string|null
+     */
+    protected $profile;
+
+    /**
+     * Source environment domain URL.
+     *
+     * @var string|null
+     */
+    protected $oldURL;
+
+    /**
+     * Target domain URL.
+     *
+     * @var string|null
+     */
+    protected $newURL;
 
     /**
      * Execute the console command.
@@ -28,31 +133,113 @@ class MigrateCommand extends Command
      */
     public function handle()
     {
-        $source = $this->argument('source');
-        $target = $this->argument('target');
+        $this->source = $this->argument('source');
+        $this->target = $this->argument('target');
 
-        $namespace = "hale-platform-$source";
-        $podName = rtrim(shell_exec("kubectl get pods -n $namespace -o=name | grep -m 1 wordpress | sed 's/^.\{4\}//'"));
-        $podExec = "kubectl exec -it -n $namespace -c wordpress pod/$podName --";
-        $sqlFile = 'hale-platform-' . $source . '-' . date("Y-m-d-H-i-s") . '.sql';
-        $path = rtrim(shell_exec('pwd'));
+        $this->namespace = "hale-platform-$this->source";
+        $this->podName = rtrim(shell_exec("kubectl get pods -n $this->namespace -o=name | grep -m 1 wordpress | sed 's/^.\{4\}//'"));
+        $this->podExec = "kubectl exec -it -n $this->namespace -c wordpress pod/$this->podName --";
+        $this->sqlFile = 'hale-platform-' . $this->source . '-' . date("Y-m-d-H-i-s") . '.sql';
+        $this->path = rtrim(shell_exec('pwd'));
 
-        # Export DB from RDS to container
-        $this->task("Export $source database from RDS", function () use ($podExec, $sqlFile) {
-            passthru("$podExec wp db export --porcelain $sqlFile", $resultCode);
+        $this->secretName = rtrim(shell_exec("kubectl describe -n $this->namespace pods/$this->podName | grep -o 'wpsecrets-[[:digit:]]*'"));
+        $this->secrets = shell_exec("cloud-platform decode-secret -n $this->namespace -s $this->secretName");
+        $this->json_secrets = json_decode($this->secrets);
+        $this->bucket = $this->json_secrets->data->S3_UPLOADS_BUCKET;
+        $this->uploadsPath = $this->path . "/wordpress/wp-content";
+        $this->profile = "$this->namespace-s3";
+        $this->oldURL = "$this->namespace.apps.live.cloud-platform.service.justice.gov.uk";
+        $this->newURL = 'hale.docker';
 
-            $resultCode = ($resultCode === 0) ? true : false;
-            return $resultCode;
-        });
+        // Migrating from CloudPlatform to local Docker site
+        if ($this->target === 'local') {
+            $this->migrateToLocalEnv(
+                $this->podExec,
+                $this->source,
+                $this->sqlFile,
+                $this->namespace,
+                $this->podName,
+                $this->path,
+                $this->target,
+                $this->secretName,
+                $this->secrets,
+                $this->json_secrets,
+                $this->bucket,
+                $this->uploadsPath,
+                $this->profile,
+                $this->oldURL,
+                $this->newURL
+            );
+            return;
+        }
 
-        passthru("kubectl cp -n $namespace -c wordpress $podName:$sqlFile $sqlFile");
+        // Migrating between CloudPlatform environments
+        if (in_array($this->target, ['prod', 'staging', 'dev', 'demo'])) {
+            $this->migrateBetweenCloudPlatformEnv(
+                $this->podExec,
+                $this->source,
+                $this->sqlFile,
+                $this->namespace,
+                $this->podName,
+                $this->path,
+                $this->target,
+                $this->secretName,
+                $this->secrets,
+                $this->json_secrets,
+                $this->bucket,
+                $this->uploadsPath,
+                $this->profile,
+                $this->oldURL,
+                $this->newURL
+            );
+            return;
+        }
 
+        $this->info('Target environment not found.');
+    }
+
+        /**
+         * Migrates the data from the specified source to the local environment.
+         *
+         * @param string $podExec The pod execution command.
+         * @param string $source The source of the database.
+         * @param string $sqlFile The SQL file name.
+         * @param string $namespace The namespace.
+         * @param string $podName The pod name.
+         * @param string $path The path.
+         * @return void
+         */
+        public function migrateToLocalEnv(
+            $podExec,
+            $source,
+            $sqlFile,
+            $namespace,
+            $podName,
+            $path,
+            $target,
+            $secretName,
+            $secrets,
+            $json_secrets,
+            $bucket,
+            $uploadsPath,
+            $profile,
+            $oldURL,
+            $newURL
+        ) {
+        
+        // Migration to local specific variables depends on Docker
         $containerID = rtrim(shell_exec('docker ps -aqf "name=^wordpress$"'));
         $containerExec = "docker exec -it wordpress";
 
-        # Get URLs to run WP find and replace on database
-        $oldURL = "$namespace.apps.live.cloud-platform.service.justice.gov.uk";
-        $newURL = 'hale.docker';
+        // Export SQL from RDS to pod container
+        $resultCode = $this->exportRdsToContainer($this->podExec, $this->source, $this->sqlFile);
+
+        if (!$resultCode) {
+            $this->handleFailure("Failed to export $source database from RDS. Exiting task.");
+            return;
+        }
+
+        passthru("kubectl cp -n $namespace -c wordpress $podName:$sqlFile $sqlFile");
 
         # Copy SQL from local machine to container
         $this->task("Copying database from local machine to target container.", function () use ($containerID, $sqlFile) {
@@ -109,13 +296,6 @@ class MigrateCommand extends Command
             return $resultCode;
         });
 
-        $secretName = rtrim(shell_exec("kubectl describe -n $namespace pods/$podName | grep -o 'wpsecrets-[[:digit:]]*'"));
-        $secrets = shell_exec("cloud-platform decode-secret -n $namespace -s $secretName");
-        $json_secrets = json_decode($secrets);
-        $bucket = $json_secrets->data->S3_UPLOADS_BUCKET;
-        $uploadsPath = $path . "/wordpress/wp-content";
-        $profile = "$namespace-s3";
-
         $this->task("Sync s3 $source bucket with local.", function () use ($bucket, $uploadsPath, $profile) {
             passthru("aws s3 sync --quiet --profile $profile s3://$bucket $uploadsPath", $resultCode);
 
@@ -125,13 +305,80 @@ class MigrateCommand extends Command
     }
 
     /**
-     * Define the command's schedule.
+     * Migrate between CloudPlatform environments
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param string $podExec
+     * @param string $source
+     * @param string $sqlFile
+     * @param string $namespace
+     * @param string $podName
+     * @param string $path
+     * @param string $target
+     * @param string $secretName
+     * @param string $secrets
+     * @param object $json_secrets
+     * @param string $bucket
+     * @param string $uploadsPath
+     * @param string $profile
      * @return void
      */
-    public function schedule(Schedule $schedule): void
-    {
-        // $schedule->command(static::class)->everyMinute();
+    public function migrateBetweenCloudPlatformEnv(
+        $podExec,
+        $source,
+        $sqlFile,
+        $namespace,
+        $podName,
+        $path,
+        $target,
+        $secretName,
+        $secrets,
+        $json_secrets,
+        $bucket,
+        $uploadsPath,
+        $profile,
+        $oldURL,
+        $newURL
+    ) {
+
+        // Export SQL from RDS to pod container
+        $resultCode = $this->exportRdsToContainer($this->podExec, $this->source, $this->sqlFile);
+
+        if (!$resultCode) {
+            $this->handleFailure("Failed to export $source database from RDS. Exiting task.");
+            return;
+        }
+
+        echo "Migrating from $source to $target.";
     }
+
+    /**
+     * Handle the failure by displaying an error message and exiting the task.
+     *
+     * @param string $errorMessage
+     * @return void
+     */
+    private function handleFailure($errorMessage)
+    {
+        $this->info($errorMessage);
+    }
+
+    /**
+     * Export the database from RDS to the container.
+     *
+     * @param string $podExec   The pod exec command.
+     * @param string $source    The source database.
+     * @param string $sqlFile   The SQL file path.
+     * @return bool             True if the export is successful; otherwise, false.
+     */
+    private function exportRdsToContainer($podExec, $source, $sqlFile)
+    {
+        $resultCode = $this->task("Export $source database from RDS", function () use ($podExec, $sqlFile) {
+            passthru("$podExec wp db export --porcelain $sqlFile", $resultCode);
+            $resultCode = ($resultCode === 0) ? true : false;
+            return $resultCode;
+        });
+
+        return $resultCode;
+    }
+
 }
